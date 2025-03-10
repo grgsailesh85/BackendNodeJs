91451985229552715203ab320fdaa59ab8cd388a
Sailesh Gurung <saileshgurung98@gmail.com>
2025-03-10 10:25:45 +0545

    update

---
+++ nodejs/index.js
@@ -0,0 +1,8 @@
+// const utils = require("./utils")
+// import utils from "./utils.js"  //for export default
+import { cube } from "./utils.js"  // for export only
+
+// const result = utils.cube(5)  // for export default 
+const result = cube(5)  // for export only
+
+console.log(result)
