623c8c25b7d90f56595fc1a5b7137f50d9522af4
Sailesh Gurung <saileshgurung98@gmail.com>
2025-04-26 15:42:17 +0545

    hbs complete

--- nodejs/src/controllers/viewController.js
+++ nodejs/src/controllers/viewController.js
@@ -1,11 +1,27 @@
+import productService from "../services/productService.js";
+
 const homePage = (req, res) => {
-  res.render("home", {
-    username: "Sailesh Gurung",
-  });
+  res.render("home", { username: "James" });
 };
 
-const productsPage = (req, res) => {
-  res.render("products");
+const productsPage = async (req, res) => {
+  const products = await productService.getAllProducts(req.query);
+
+  res.render("products", { products });
 };
 
-export { homePage, productsPage };
+const aboutPage = (req, res) => {
+  res.render("about", { username: "Sailesh Gurung" });
+};
+
+const contactPage = (req, res) => {
+  res.render("contact", { username: "Sailesh Gurung" });
+};
+
+const productByIdPage = async (req, res) => {
+  const product = await productService.getProductById(req.params.id);
+
+  res.render("product", { product });
+};
+
+export { homePage, productsPage,aboutPage, contactPage , productByIdPage };
--- nodejs/src/routes/viewRoute.js
+++ nodejs/src/routes/viewRoute.js
@@ -1,9 +1,22 @@
 import express from "express";
-import { homePage, productsPage } from "../controllers/viewController.js";
+import {
+  aboutPage,
+  contactPage,
+  homePage,
+  productByIdPage,
+  productsPage,
+} from "../controllers/viewController.js";
 
 const router = express.Router();
 
 router.get("/home", homePage);
+
 router.get("/products", productsPage);
+
+router.get("/about", aboutPage);
+
+router.get("/contact", contactPage);
+
+router.get("/products/:id", productByIdPage);
 
 export default router;
